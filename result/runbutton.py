6110990bef725bfc22bdfc8b1063d88ca8e96453
lee090626 <hyeongseogi46@gmail.com>
2025-06-16 13:34:33 +0900

    계속하기 버그 고침

--- result/runbutton.py
+++ result/runbutton.py
@@ -13,34 +13,50 @@ class RunButton(StudyDamagochiFrame):
         self.frame.bstop.config(command=self.일시정지)
         self.frame.breplay.config(command=self.계속)
 
-        self.sec=0
-        self.playing=False
+        self.sec = 0
+        self.playing = False
         self.stoping = False
+        self.timer_id = None
+        
 
-    def 공부시작(self): # 여러번 누르면 왜 누적되내
-        if not self.playing and not self.stoping:   # 타이머가 실행중이 아닐 때만 시작
+    def 공부시작(self):
+        if not self.playing and not self.stoping:
             self.playing = True
+            self.stoping = False
+            if self.timer_id:  # 이전 타이머가 있다면 취소
+                self.frame.win.after_cancel(self.timer_id)
+                self.timer_id = None
+            self.타이머업데이트()
+
+    def 계속(self):
+        if not self.playing and self.stoping:
+            self.playing = True
+            self.stoping = False
+            if self.timer_id:  # 이전 타이머가 있다면 취소
+                self.frame.win.after_cancel(self.timer_id)
+                self.timer_id = None
             self.타이머업데이트()
 
     def 공부종료(self):
         self.playing = False
+        self.stoping = False
         self.sec = 0
         self.frame.timer.config(text="00:00")
+        if self.timer_id:  # 타이머 중단
+            self.frame.win.after_cancel(self.timer_id)
+            self.timer_id = None
 
     def 일시정지(self):
         self.playing = False
         self.stoping = True
-
-    def 계속(self):
-        if not self.playing:
-            self.playing = True
-            self.stoping = False
-            self.타이머업데이트()
-
+        if self.timer_id:
+            self.frame.win.after_cancel(self.timer_id)
+            self.timer_id = None
+            
     def 타이머업데이트(self):
         if self.playing:
             self.sec += 1
-            self.minutes = self.sec // 60
-            self.seconds = self.sec % 60
-            self.frame.timer.config(text=f"{self.minutes:02}:{self.seconds:02}")
-            self.frame.win.after(1000, self.타이머업데이트)
+            minutes = self.sec // 60
+            seconds = self.sec % 60
+            self.frame.timer.config(text=f"{minutes:02}:{seconds:02}")
+            self.timer_id = self.frame.win.after(1000, self.타이머업데이트)
